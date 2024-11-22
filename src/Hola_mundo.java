be68003a7fb45b01baf7ee1136b0a0a9c144ab7d
ana131 <antakarana131@gmail.com>
2024-11-22 12:05:47 +0100

    Añadido mensaje de bienvenida

--- src/Hola_mundo.java
+++ src/Hola_mundo.java
@@ -1,6 +1,8 @@
 public class Hola_mundo {
     public static void main(String[] args) {
         System.out.println("Hola, mundo desde IntelliJ IDEA!\"");
+        System.out.println("Bienvenidos al curso de DAM");
+
     }
 
 }
